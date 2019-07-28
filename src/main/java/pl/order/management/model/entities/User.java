5e370941e6cead5c23a83994558a5ab843700105
Michal Motyka <motyka82@gmail.com>
2019-07-28 14:37:15 +0200

    Michal - User class getters, setters...

--- src/main/java/pl/order/management/model/entities/User.java
+++ src/main/java/pl/order/management/model/entities/User.java
@@ -1,8 +1,11 @@
 package pl.order.management.model.entities;
 
+import java.util.Objects;
+
 public class User {
 
     private Long id;
+    private String username;
     private String firstName;
     private String lastName;
     private Department departmentId;
@@ -10,5 +13,92 @@ public class User {
     private Boolean active;
     private Position positionId;
 
+    public Long getId() {
+        return id;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Department getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Department departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Position getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(Position positionId) {
+        this.positionId = positionId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("User{");
+        sb.append("id=").append(id);
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", departmentId=").append(departmentId);
+        sb.append(", eMail='").append(eMail).append('\'');
+        sb.append(", active=").append(active);
+        sb.append(", positionId=").append(positionId);
+        sb.append('}');
+        return sb.toString();
+    }
 }
