b4529c552c471e896bc2bf007853d7b6639bc895
Katarzyna Kmieć <katarzyna.kmiec93@gmail.com>
2019-07-28 14:27:33 +0200

    Kasia - Order podstawa

--- src/main/java/pl/order/management/model/entities/Order.java
+++ src/main/java/pl/order/management/model/entities/Order.java
@@ -1,4 +1,18 @@
 package pl.order.management.model.entities;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class Order {
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private User userId;
+    private Department departmentId;
+    private LocalDateTime dateTime;
+
+
+
+
 }
