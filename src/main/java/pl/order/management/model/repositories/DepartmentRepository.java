3642cf827039c883f71fb3cc0c71612ca6dc7535
Katarzyna Kmieć <katarzyna.kmiec93@gmail.com>
2019-08-03 12:13:16 +0200

    Kasia- repozytoria User i Order

--- src/main/java/pl/order/management/model/repositories/DepartmentRepository.java
+++ src/main/java/pl/order/management/model/repositories/DepartmentRepository.java
@@ -1,4 +1,10 @@
 package pl.order.management.model.repositories;
 
-public class DepartmentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.order.management.model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentRepository extends JpaRepository<Department,Long> {
+// TODO możliwe do usunięcia
 }
--- src/main/java/pl/order/management/model/repositories/UserRepository.java
+++ src/main/java/pl/order/management/model/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package pl.order.management.model.repositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.order.management.model.entities.User;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    List<User> findByUsername(String username);
+
+    User findUserByUsername(String username);
 }
