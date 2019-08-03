40893ee3d8f53ed172abc5ab0e595946cf63cd56
Katarzyna Kmieć <katarzyna.kmiec93@gmail.com>
2019-08-03 14:39:17 +0200

    Kasia - PositionController

--- src/main/java/pl/order/management/controllers/PositionController.java
+++ src/main/java/pl/order/management/controllers/PositionController.java
@@ -1,8 +1,33 @@
 package pl.order.management.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.order.management.model.entities.Position;
+import pl.order.management.model.repositories.PositionRepository;
 
 @Controller
+@RequestMapping("/position")
 public class PositionController {
 
+    private PositionRepository positionRepository;
+
+    @Autowired
+    public PositionController(PositionRepository positionRepository) {
+        this.positionRepository = positionRepository;
+    }
+
+    @GetMapping
+    public String processPositionAddPage() {
+        return "add-position";
+    }
+    @PostMapping
+    public String addPosition(String name){
+        Position position = new Position();
+        position.setPositions(name);
+        positionRepository.save(position);
+        return "redirect:/";
+    }
 }
--- src/main/java/pl/order/management/model/repositories/PositionRepository.java
+++ src/main/java/pl/order/management/model/repositories/PositionRepository.java
@@ -1,9 +1,10 @@
 package pl.order.management.model.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.order.management.model.entities.Position;
 
-
+@Repository
 public interface PositionRepository extends JpaRepository<Position,Long> {
 // TODO możliwe do usunięcia
 }
