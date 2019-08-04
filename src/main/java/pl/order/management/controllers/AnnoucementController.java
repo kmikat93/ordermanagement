ac4f7e600ba584e216fa09e40fedd4f930b56f7a
Michal Motyka <motyka82@gmail.com>
2019-08-04 10:52:22 +0200

    Michal - added Annoucement class, and repo as TODO

---
+++ src/main/java/pl/order/management/controllers/AnnoucementController.java
@@ -0,0 +1,6 @@
+package pl.order.management.controllers;
+
+public class AnnoucementController {
+
+    //TODO kontroler do ogloszen
+}
---
+++ src/main/java/pl/order/management/model/entities/Announcement.java
@@ -0,0 +1,9 @@
+package pl.order.management.model.entities;
+
+public class Announcement {
+
+    //TODO przerobic na encje
+    private Long id;
+    private String title;
+    private String description;
+}
